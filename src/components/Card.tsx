3fcb44488e558ce4968bf16104ddc32ebc4e7f54
VictorGMSS <victors.study@gmail.com>
2023-12-27 15:33:04 -0300

    ATT 1.01

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ const Card: FC<CardProps> = ({ imgSrc, title }) => {
   return (
     <div
       className="relative group flex items-center justify-center py-12 px-8 bg-accent
-     hover:bg-hoverPrimary flex-1 cursor-pointer h-[220px] xs:h-[290px] min-w-fit sm:min-w-[330px]
+     hover:bg-hoverPrimary flex-1 cursor-pointer h-[240px] xs:h-[290px] min-w-fit sm:min-w-[330px]
       max-w-[448px] rounded-2xl overflow-hidden transition-all duration-700 ease-in-out"
     >
       <img
